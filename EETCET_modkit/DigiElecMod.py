f2522c13b89b42916ba7dda374970598a351c82d
Chris Amedjonekou <death.thestraw.berry@gmail.com>
2018-11-07 22:18:07 -0500

    11/07/18

--- EETCET_modkit/DigiElecMod.py
+++ EETCET_modkit/DigiElecMod.py
@@ -37,6 +37,35 @@ def dec_to_diffBase(number, base= 2):
             binString = ''.join(str(i) for i in binList)
             return binString
 
+def diffBase_to_dec(number, base= 2):
+    """This function converts numbers from other number systems to base 10 (decimal).
+    [E.g.: base 2 (bin), base 16 (hex), base 8 (octal)]. By default it converts from bin to dec.
+    It takes in one argument, number, which is expected to be other number systems.
+    It returns the number as a base 10 number."""
+    number = list(str(number))
+    number.reverse()
+    decList = []
+    for i in range(0,len(number)):
+
+        if base == 16:
+            if number[i] == 'a':
+                number[i] = 10
+            elif number[i] == 'b':
+                number[i] = 11
+            elif number[i] == 'c':
+                number[i] = 12
+            elif number[i] == 'd':
+                number[i] = 13
+            elif number[i] == 'e':
+                number[i] = 14
+            elif number[i] == 'f':
+                number[i] = 15
+
+        digit = int(number[i]) * m.pow(base, i)
+        decList.append(digit)
+    arr1 = np.array(decList)
+    return int(np.sum(arr1))
+
 def hex_to_bin(number, base= 16):
     """This function converts numbers from hex to base 2 (bin).
     It takes in one argument, number, which is expected to be hex.
