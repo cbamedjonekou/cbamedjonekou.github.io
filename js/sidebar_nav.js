165b46f450c1cf11a46d9add439836a912de19a1
Chris Amedjonekou <death.thestraw.berry@gmail.com>
2019-11-19 11:06:22 -0500

    switched positions btw/ openNav vs. closeNav

--- js/sidebar_nav.js
+++ js/sidebar_nav.js
@@ -1,15 +1,15 @@
+function closeNav() {
+  document.getElementById("whole-sidebar-nav").style.width = "0";
+  document.getElementById("main-content").style.marginLeft= "0";
+  document.getElementById("footer-content").style.marginLeft= "0";
+  document.getElementById("header-dnstsu").style.marginLeft= "0";
+  document.body.style.backgroundColor = "black";
+}
+
 function openNav() {
-    document.getElementById("whole-sidebar-nav").style.width = "300px";
-    document.getElementById("main-content").style.marginLeft = "300px";
-    document.getElementById("footer-content").style.marginLeft = "300px";
-    document.getElementById("header-dnstsu").style.marginLeft = "300px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
-  }
-  
-  function closeNav() {
-    document.getElementById("whole-sidebar-nav").style.width = "0";
-    document.getElementById("main-content").style.marginLeft= "0";
-    document.getElementById("footer-content").style.marginLeft= "0";
-    document.getElementById("header-dnstsu").style.marginLeft= "0";
-    document.body.style.backgroundColor = "black";
-  }
+  document.getElementById("whole-sidebar-nav").style.width = "300px";
+  document.getElementById("main-content").style.marginLeft = "300px";
+  document.getElementById("footer-content").style.marginLeft = "300px";
+  document.getElementById("header-dnstsu").style.marginLeft = "300px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
+}
